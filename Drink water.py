03e936a6850f05459ca880d7d7eaf778cc85ccfd
jitendra lodhi <jitendralodhi108@gmail.com>
2021-04-25 14:08:20 +0530

    first commit

---
+++ Drink water.py
@@ -0,0 +1,17 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    while True:
+        notification.notify(
+        title = "please Drink Water.....! ",
+        message = "\n Whiskey is for drinking; water is for fighting over. 😀🥛",
+        app_icon = "watericon.ico",
+        timeout= 12
+        )
+        time.sleep(60*60)
+
+        # note : if you run this program using pythonw.Drink water.py it will run even after closing the terminal in particular
+        # time that has been set in the program 
+
+   
